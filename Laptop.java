9477b0a3e258acfe5b2ea22ace5afa30cd449136
César Higashi <76760288+higashi04@users.noreply.github.com>
2022-07-04 22:25:38 -0500

    creacion de clases

---
+++ Laptop.java
@@ -0,0 +1,18 @@
+public class Laptop {
+    public static void Modelo(){
+        System.out.println("Modelo Asus VivoBook 15");
+    }
+    public static void Ram(){
+         System.out.println("RAM: RAM de 16 gigabytes");
+    }
+    public static void Cores(){
+        System.out.println("CPU de 8 cores");
+    }
+    public static void TarjetaGrafica() {
+        System.out.println("Tarjeta Ryzen 7 con graficos AMD RADEON");
+    }
+    public static void HuellaDigital() {
+        System.out.println("Lector de huella digital que no funciona desde que windows actualizó drivers.");
+    }
+}
+
---
+++ Programa.java
@@ -0,0 +1,10 @@
+// import java.io.*;
+class Programa {
+    public static void main(String[] args) {
+        Laptop.Modelo();
+        Laptop.Ram();
+        Laptop.Cores();
+        Laptop.TarjetaGrafica();
+        Laptop.HuellaDigital();
+    }
+}
